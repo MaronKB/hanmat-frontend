0cce2213cdbbe9b29a01bc919ccb509673d12f2a
minsuleefromnowhere <cw01094@gmail.com>
2024-12-27 14:56:49 +0900

    로그인 페이지

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import React from 'react';
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from './routes';
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <AppRoutes />
+  </BrowserRouter>,
 )
