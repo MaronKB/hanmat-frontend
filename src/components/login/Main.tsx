23d01dab05b8f8c08f3e3ed6ae19d5cfd5e9d003
MRKB <asdfgh7913@naver.com>
2024-12-28 01:20:03 +0900

    route

--- src/components/login/Main.tsx
+++ src/components/login/Main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
 // import hanmatLogo from '../../assets/hanmatLogo.svg';
 // import '../styles/LoginPage.css';
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import { createRoot } from 'react-dom/client'
 import Nav from './components/nav/Main.tsx';
+import Home from './components/home/Main.tsx';
+import Login from './components/login/Main.tsx';
 import './index.css'
 import "./locales/i18n.ts";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <Nav/>
+        <Routes>
+            <Route element={<Nav />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Route>
+        </Routes>
     </BrowserRouter>
 )
