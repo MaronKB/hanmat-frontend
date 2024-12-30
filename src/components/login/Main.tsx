f53107d01e2cd1697cd7db03fbd1b4966bb318fc
minsuleefromnowhere <cw01094@gmail.com>
2024-12-30 15:18:12 +0900

    login css

--- src/components/login/Main.tsx
+++ src/components/login/Main.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-// import hanmatLogo from '../../assets/hanmatLogo.svg';
-// import '../styles/LoginPage.css';
+import styles from './Main.module.css';
 
 const Login = () => {
     const navigate = useNavigate();
@@ -14,20 +13,19 @@ const Login = () => {
     };
 
     return (
-        <div className="login-page">
-            <div className="login-container">
-                {/*<img src={hanmatLogo} alt="HANMAT Logo" className="logo" />*/}
-                <h1 className="title">HANMAT</h1>
-                <p className="welcome-message">Welcome</p>
-                <p className="login-prompt">
-                    Log in to HANMAT to continue to HANMAT FIND.
-                </p>
-                <button className="google-login-button" onClick={googleLogin}>
-                    <span className="google-icon"></span>
-                    <span className="button-text">Continue with Google</span>
+        <div className={styles.loginPage}>
+            <div className={styles.loginContainer}>
+                {/* <img src={logo} alt="HANMAT Logo" className={styles.logo} /> */}
+                <h1 className={styles.title}>HANMAT</h1>
+                <p className={styles.welcomeMessage}>Welcome</p>
+                <p className={styles.loginPrompt}>Log in to HANMAT to continue to HANMAT FIND.</p>
+                <hr className={styles.separator}/>
+                <button className={styles.googleLoginButton} onClick={googleLogin}>
+                    <span className={styles.googleIcon}></span>
+                    <span className={styles.buttonText}>Continue with Google</span>
                 </button>
             </div>
-            <button className="back-button" onClick={goBack}>
+            <button className={styles.backButton} onClick={goBack}>
                 Back
             </button>
         </div>
