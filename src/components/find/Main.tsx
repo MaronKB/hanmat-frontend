61c0b036a072fcbc62802d4d5a0f9c19d04e8d12
MRKB <64984647+MaronKB@users.noreply.github.com>
2024-12-31 12:26:05 +0900

    찾기 페이지 기초

--- src/components/find/Main.tsx
+++ src/components/find/Main.tsx
@@ -1,7 +1,39 @@
+import {Container as MapDiv, NaverMap, Marker, NavermapsProvider} from "react-naver-maps";
+import styles from "./Main.module.css";
+import {useEffect, useState} from "react";
+
 export default function Main() {
+    let lang = localStorage.getItem("lang") || "en";
+    if (lang === "jp") lang = "ja";
+
+    const [userLocation, setUserLocation] = useState({lat: 37.5665, lng: 126.9780});
+
+    useEffect(() => {
+        getMyLocation();
+    }, []);
+
+    useEffect(() => {
+    }, [userLocation]);
+
+    const getMyLocation = () => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            const coords = {
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude
+            };
+            setUserLocation(coords);
+            console.log(pos);
+        });
+    };
     return (
-        <div>
-            <h1>Find</h1>
-        </div>
+        <NavermapsProvider ncpClientId={"d2682gqz4u&language=" + lang}>
+            <main className={"max " + styles.main}>
+                <MapDiv style={{width: "100%", height: "100%"}}>
+                    <NaverMap defaultCenter={userLocation} zoom={18}>
+                        <Marker defaultPosition={userLocation}/>
+                    </NaverMap>
+                </MapDiv>
+            </main>
+        </NavermapsProvider>
     );
 }
