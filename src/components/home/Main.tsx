90790157434c66001034fed99c34b06c8c366086
MRKB <asdfgh7913@naver.com>
2025-01-11 17:03:34 +0900

    메인 페이지 디자인

--- src/components/home/Main.tsx
+++ src/components/home/Main.tsx
@@ -1,31 +1,24 @@
 import {useTranslation} from "react-i18next";
 import styles from './Main.module.css';
-import logo from '../../assets/hanmat_logo.png';
-// import homeBg from '../../assets/images/home-bg.jpg';
+import bg from '../../assets/images/home-bg.jpg';
 import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
 
 export default function Main() {
     const {t} = useTranslation("home");
     return (
-        <main className={styles.main} /*style={{backgroundImage : `url(${homeBg})`}}*/>
-            <img src={logo} alt="HANMAT"/>
-            <h2 className={styles.title}>{t("home:welcome")}</h2>
-            <section className={styles.section}>
-                <h3>{t("home:section1")}</h3>
-                <p>{t("home:section1Content")}</p>
-                <div className={styles.buttons}>
-                    <Link to={"/login"}>{t("home:getStarted")}</Link>
-                    <Link to={"/find"}>{t("home:learnMore")}</Link>
-                </div>
-            </section>
-            <section className={styles.section}>
-                <h3>{t("home:section2")}</h3>
-                <p>{t("home:section2Content")}</p>
-            </section>
-            <section className={styles.section}>
-                <h3>{t("home:section3")}</h3>
-                <p>{t("home:section3Content")}</p>
-            </section>
+        <main className={styles.main} style={{backgroundImage: `url(${bg})`}}>
+            {/*<img src={logo} alt="HANMAT"/>*/}
+            <h2 className={styles.title}>HANMAT</h2>
+            <div className={styles.description}>
+                <h3>{t("home:title")}</h3>
+                <p>{t("home:description")}</p>
+            </div>
+            <div className={styles.buttons}>
+                <h3>{t("home:learnMore")}</h3>
+                <Link to={"/login"}><FontAwesomeIcon icon={faArrowUpRightFromSquare}/></Link>
+            </div>
         </main>
     );
 }
